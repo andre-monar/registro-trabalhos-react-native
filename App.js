b6703221591f6bd828afcd85afab00cf35a44e3f
andre <081230012@faculdade.cefsa.edu.br>
2026-03-18 20:42:31 -0300

    Commit inicial - Banco de dados

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, } from 'react-native';
+import { useEffect, useState } from 'react';
+import { createTables } from './database/Database.js';
+import styles from './styles';
+
 
 export default function App() {
+  const [status, setStatus] = useState('Aguardando...');
+
+
+  // Inicializar banco ao abrir o app
+  const setup = async () => {
+        setStatus('Criando banco...');
+        await createTables();
+        setStatus('Banco criado! ✅');
+    };
+
+    useEffect(() => {
+        setup();
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>{status}</Text> 
+        <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ database/Database.js
@@ -0,0 +1,68 @@
+import * as SQLite from 'expo-sqlite';
+
+// criar ou abrir db
+export async function getDbConnection() {
+    return await SQLite.openDatabaseAsync('dbRegistroTrabalhos.db');
+}
+
+// criar tabelas
+export async function createTables() {
+    var cx = await getDbConnection();
+    try {
+        const queryAluno = `CREATE TABLE IF NOT EXISTS tbAluno
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ra TEXT NOT NULL UNIQUE,
+            nome TEXT NOT NULL          
+        )`;
+        await cx.execAsync(queryAluno);
+
+        const queryTrabalho = `CREATE TABLE IF NOT EXISTS tbTrabalho
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            descricao TEXT,
+            data_entrega DATE,       
+            situacao TEXT CHECK(situacao IN ('Pendente', 'Concluído', 'Cancelado'))  
+        )`;
+        await cx.execAsync(queryTrabalho);
+
+        const queryAtividade = `CREATE TABLE IF NOT EXISTS tbAtividade
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            descricao TEXT,
+            horas_previstas REAL,
+            horas_concluidas REAL,
+            situacao TEXT CHECK(situacao IN ('Pendente', 'Concluído', 'Cancelado'))      
+        )`;
+        await cx.execAsync(queryAtividade);
+
+        const queryAtividadeAluno = `CREATE TABLE IF NOT EXISTS tbAtividadeAluno
+        (
+            idAtividade INTEGER,
+            idAluno INTEGER,
+            FOREIGN KEY (idAtividade) REFERENCES tbAtividade(id),
+            FOREIGN KEY (idAluno) REFERENCES tbAluno(id),
+            PRIMARY KEY (idAtividade, idAluno)   
+        )`;
+        await cx.execAsync(queryAtividadeAluno);
+
+        const queryTrabalhoAluno = `CREATE TABLE IF NOT EXISTS tbTrabalhoAluno
+        (
+            idTrabalho INTEGER,
+            idAluno INTEGER,
+            FOREIGN KEY (idTrabalho) REFERENCES tbTrabalho(id),
+            FOREIGN KEY (idAluno) REFERENCES tbAluno(id),
+            PRIMARY KEY (idTrabalho, idAluno)  
+        )`;
+        await cx.execAsync(queryTrabalhoAluno);
+
+        console.log("Banco aberto com sucesso!");
+    } catch (erro) {
+        console.log("Erro ao criar banco: ", erro);
+    }
+    finally {
+        await cx.closeAsync();
+    }
+};
